7f39119ec7663b3592b5f2743f373d997e2dc7c3
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-03-14 22:41:35 -0700

    add append method and tail method

--- linked-list.rb
+++ linked-list.rb
@@ -5,6 +5,21 @@ class LinkedList
         @tail = nil
     end
 
+    def append(value)
+        if @head.nil?
+            @head = Node.new(value)
+            return @tail = tail
+        end
+        @tail.next_node = Node.new(value)
+        @tail = @tail.next_node
+    end
+
+    def tail(node = @head)
+        return node if node.nil? || node.next_node.nil?
+        current_node = node.next_node
+        tail(current_node)
+    end
+
 end
 
 class Node
@@ -15,3 +30,9 @@ class Node
         @next_node = next_node
     end
 end
+
+list = LinkedList.new
+p list.append('test')
+p list.append('test2')
+p list.append('test3')
+p list.tail
