6573b7c2f795c93ea5abd7e5aa4b174cea2b256d
Augelvin Theone Dermaga <augelvintd@gmail.com>
2023-03-15 19:53:58 -0700

    add size method

--- linked-list.rb
+++ linked-list.rb
@@ -1,29 +1,38 @@
 class LinkedList
 
-    def initialize
-        @head = nil
-        @tail = nil
-    end
+  def initialize
+    @head = nil
+    @tail = nil
+  end
 
-    def append(value)
-        if @head.nil?
-            @head = Node.new(value)
-            return @tail = tail
-        end
-        @tail.next_node = Node.new(value)
-        @tail = @tail.next_node
+  def append(value)
+    if @head.nil?
+      @head = Node.new(value)
+      return @tail = tail
     end
+    @tail.next_node = Node.new(value)
+    @tail = @tail.next_node
+  end
 
-    def prepend(value)
-        @head = Node.new(value, @head.next_node)
+  def prepend(value)
+    @head = Node.new(value, @head.next_node)
+  end
+
+  def size
+    counter = 1
+    node = @head
+    until node.next_node.nil? do
+      counter += 1
+      node = node.next_node
     end
+    counter
+  end
 
-
-    def tail(node = @head)
-        return node if node.nil? || node.next_node.nil?
-        current_node = node.next_node
-        tail(current_node)
-    end
+  def tail(node = @head)
+    return node if node.nil? || node.next_node.nil?
+    current_node = node.next_node
+    tail(current_node)
+  end
 
 end
 
@@ -42,3 +51,4 @@ p list.append('test2')
 p list.append('test3')
 p list.prepend('test0')
 p list.tail
+p list.size
